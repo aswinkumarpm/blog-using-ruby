0063223ab59c40b294159e9ae68fb8dfbc547fc2
aswinkumarpm <aswinkumarpm3@gmail.com>
2020-03-17 16:12:32 +0530

    the redirection changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ def create
   if user  && user.authenticate(params[:session][:password])
    session[:user_id] = user.id
    flash[:success] = "You have logged in"
-   redirect_to users_path(user)
+   redirect_to articles_path
   else
    flash.now[:danger] = "There was something wrong with your login information"
    render 'new'
